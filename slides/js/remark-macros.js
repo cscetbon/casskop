4fb3f0f371fe86bc15eac1cbcaeada71c16a577d
AKamyshnikova <AKamyshnikova@users.noreply.github.com>
2025-07-02 13:38:16 +0000

    deploy: d88739c00b6ca1cea22b1d58e64d4fc4d6013376

--- slides/js/remark-macros.js
+++ slides/js/remark-macros.js
@@ -1,1 +1,12 @@
-remark.macros.scale=function(r){return'<img src="'+this+'" style="width: '+r+'" />'};
+/**
+ * Created by cfwr6466 on 15/09/2016.
+ */
+
+remark.macros.scale = function (percentage) {
+    var url = this;
+    return '<img src="' + url + '" style="width: ' + percentage + '" />';
+    // Usage:
+    //   ![:scale 50%](image.jpg)
+    // Outputs:
+    //   <img src="image.jpg" style="width: 50%" />
+};
